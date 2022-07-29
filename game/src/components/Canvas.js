dce5e0adb2d827e4b102010071278dff03c69461
Clement BEAURAIN <clementbeaurainpro@gmail.com>
2022-07-29 14:04:48 +0200

    rien de nouveau

--- game/src/components/Canvas.js
+++ game/src/components/Canvas.js
@@ -9,25 +9,25 @@ const Canvas = () => {
 		context.fillRect(0, 0, context.canvas.width, context.canvas.height)
 	}
 	const drawDashedLine = (context) => {
-		context.lineWidth = 4;
+		context.lineWidth = 5;
 		context.strokeStyle = 'white'
 		context.lineCap = 'square'
-		context.setLineDash([4, 8]);/*dashes are 5px and spaces are 3px*/
+		context.setLineDash([5, 10]);/*dashes are 5px and spaces are 3px*/
 		context.beginPath();
-		context.moveTo(150, 1);
-		context.lineTo(150, 300);
+		context.moveTo(20.5, 0);
+		context.lineTo(20.5, 250);
 		context.stroke();
 	}
   useEffect(() => {
     const canvas = canvasRef.current
-		canvas.width = 300;
-		canvas.height = 150;
+		canvas.width = 250;
+		canvas.height = 250;
 		canvas.style.width = "90vw";
 		canvas.style.height = "90vh";
     const context = canvas.getContext('2d')
     //Our first draw
     drawBackground(context)
-		drawDashedLine(context)
+	drawDashedLine(context)
   }, [])
 
 	return (
