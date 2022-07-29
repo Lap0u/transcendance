7f83abaaeeef6b006a538ce60033f8985ffdfeca
Clement BEAURAIN <clementbeaurainpro@gmail.com>
2022-07-29 11:25:31 +0200

    canvas basique afficher mais pas responsive

---
+++ game/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Canvas from './components/Canvas'
+
+function App() {
+  return (
+	<Canvas />
+  );
+}
+
+export default App;
---
+++ game/src/components/Canvas.js
@@ -0,0 +1,11 @@
+import './Canvas.css';
+
+const Canvas = () => {
+	return (
+		<canvas id="mainWindow">
+			There should be the canvas of the full game
+		</canvas>
+	)
+}
+
+export default Canvas;
