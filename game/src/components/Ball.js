fd832cb1fca7654e93c239542a249214186582cf
Eli <eli.azn@hotmail.com>
2022-07-29 22:26:19 +0200

    balls hiting borders adjusted and added comments

--- game/src/components/Ball.js
+++ game/src/components/Ball.js
@@ -8,18 +8,19 @@ export class Ball{
 
 	drawBall = (context, ballPosX, ballPosY, playBarPosY) => {
 		//  const cvwidth = context.canvas.width
-		console.log(ballPosX)
+		//const cvheight = context.canvas.height
+
+		//change direction if ther ball if it hits a wall
 		if (ballPosY  - (this.size / 2) <= 0  || 
-		ballPosY  - (this.size / 2) >= context.canvas.height)
+		ballPosY  + (this.size / 2) >= context.canvas.height)
 			this.dirY *= -1;
 		if (ballPosX  - (this.size / 2) <= 0  || 
-			ballPosX  - (this.size / 2) >= context.canvas.width)
-				this.dirX *= -1;
-		  const cvheight = context.canvas.height
-		  console.log(ballPosY)
+			ballPosX  + (this.size / 2) >= context.canvas.width)
+			this.dirX *= -1;
+
 		  context.fillStyle = 'white';
 		  context.beginPath();
-		  context.arc(ballPosX, ballPosY, cvheight / 30, 0, 2 * Math.PI);
+		  context.arc(ballPosX, ballPosY, this.size, 0, 2 * Math.PI);
 		  context.closePath();
 		  context.fill();
 	  }
