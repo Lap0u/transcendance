0809bdb98445366118c7dac3bc8006cebf2ee37c
Clement BEAURAIN <clementbeaurainpro@gmail.com>
2022-08-03 16:16:36 +0200

    probleme rebond

--- game/src/components/Ball.tsx
+++ game/src/components/Ball.tsx
@@ -4,9 +4,10 @@ import {getRandomArbitrary} from './Canvas.tsx'
 var today = new Date()
 
 export var ball = {
-		dirX : (today.getMilliseconds() % 2) ? getRandomArbitrary(0.1, 0.9) : getRandomArbitrary(-0.9, -0.1),
+		dirX : 0.5,
+		// dirX : (today.getMilliseconds() % 2) ? getRandomArbitrary(0.1, 0.9) : getRandomArbitrary(-0.9, -0.1),
 		dirY : getRandomArbitrary(-1, 1),
-		speed : (window.innerHeight + window.innerWidth) / 2 * 0.0006,
+		speed : (window.innerHeight + window.innerWidth) / 2 * 0.0003,
 		size : (window.innerHeight + window.innerWidth) / 2 * 0.008
 		//		drawBall : drawBall()
     }
@@ -15,15 +16,30 @@ export const drawBall = (context : any, ballPosX : number, ballPosY : number, pl
 
 	// conso
 	//change direction if ther ball if it hits a wall
+	let hitpos : number = 0
+	let barhit : number = 0
+	let angle : number = 0
 	if ((ballPosY  - ball.size <= 0 && ball.dirY < 0) || 
-		(ballPosY  + ball.size  >= context.canvas.height && ball.dirY > 0))
+		(ballPosY  + ball.size  >= context.canvas.height && ball.dirY > 0)) //hitting top or left
 		ball.dirY *= -1;
-	if (Math.round(playBar.posX) <= Math.round(ballPosX  - ball.size)
+	if (Math.round(playBar.posX) <= Math.round(ballPosX  - ball.size)//hitting left playbar
 		&& Math.round(ballPosX  - ball.size) <= Math.round(playBar.posX + playBar.width) &&
 		(ballPosY >= playBarPosY && ballPosY <= playBarPosY + playBar.height) && ball.dirX < 0)
 		{
-			console.log("touche");
-			ball.dirX *= -1;
+			hitpos = ballPosY - playBarPosY
+			barhit = (hitpos * 100 / playBar.height)
+			barhit = barhit > 80 ? 80 : barhit
+			barhit = barhit < 20 ? 20 : barhit
+			angle = barhit * 180 / 100
+			// angle = angle > 90 ? angle - 90 : 360 - angle //essayer mais marche pas
+			console.log('sin , cos' ,Math.sin(angle), Math.cos(angle));
+			
+			console.log("before", ball.dirX, ball.dirY);
+			
+			console.log(hitpos, barhit, angle);
+			ball.dirX = Math.sin(angle) * ball.speed
+			ball.dirY = Math.cos(angle) * ball.speed;
+			console.log("after", ball.dirX, ball.dirY);
 		}
 	if (ballPosX  + ball.size >= context.canvas.width - playBar.posX && ball.dirX > 0)//fake bar
 		ball.dirX *= -1;
