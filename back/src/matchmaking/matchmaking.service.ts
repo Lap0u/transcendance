f53b344fdc776b7c43e20f224cd6c9c5467bf938
Clement Beaurain <clementbeaurainpro@gmail.com>
2022-08-16 23:39:22 +0200

    probleme pour emit sur le socket pour le MM, le socket est probablement pas init dans le back

--- back/src/matchmaking/matchmaking.service.ts
+++ back/src/matchmaking/matchmaking.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { matchmakingDto, joinMatchmakingDto } from './matchmaking.dto';
+import { SocketService } from 'src/socket/socket.service';
 import { v4 as uuid} from 'uuid';
 
 @Injectable()
 export class MatchmakingService {
-	private  matchmakingList : matchmakingDto[] = []
+		private  matchmakingList : matchmakingDto[] = []
+		private socketService: SocketService
 	
 	getMatchmakingList() : matchmakingDto[] {
 		return this.matchmakingList
@@ -16,6 +18,9 @@ export class MatchmakingService {
 			...payload
 		}
 		this.matchmakingList.push(newUserInMatchmaking);
+		if(this.matchmakingList.length >= 2)
+			this.socketService.socket.emit(`matchFound:`, 'We got a match');
+
 		return newUserInMatchmaking;
 	}
 	
--- game/src/components/game_menu/Game_menu.tsx
+++ game/src/components/game_menu/Game_menu.tsx
@@ -3,8 +3,12 @@ import { Button, Space, version } from "antd";
 import { useNavigate } from 'react-router-dom';
 import { v4 as uuid} from 'uuid';
 import axios from 'axios';
+import io from 'socket.io-client';
 
 const BACK_URL = "http://localhost:4000";
+
+const socket = io(BACK_URL).connect();
+
 const sessionId = uuid()
 
 const GameMenu = () => {
@@ -35,11 +39,21 @@ const GameMenu = () => {
 
   useEffect(() => {
   
-    if (inMatchmaking) {
+    if (inMatchmaking)
       joinMatchmakingList(sessionId) // id unique a ajouter dans le localstorage, utiliser un userId de l'auth 42!
-    }
-    else
+    if (!inMatchmaking)
       quitMatchmakingList(sessionId)
+    socket.on('connect', () => {
+      console.log('socket connect');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('socket disconnect');
+    });
+
+    socket.on(`matchFound`, (gameId) => {
+      console.log('gameId', gameId)
+    });
   }, [inMatchmaking]);
 
 	var matchmakingButton = inMatchmaking ? "Exit Matchmaking" : "Join Matchmaking"
