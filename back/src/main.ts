d594a909cb1d25b7751f49f81adc4c7d31206f8d
Eli <eli.azn@hotmail.com>
2022-08-20 14:51:22 +0200

    testing database

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const config: ConfigService = app.get(ConfigService);
-  const port: number = config.get<number>('PORT');
-
-  await app.listen(port, () => {
-    console.log('[BACK] listen on port ', port);
-  });
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
--- back/src/socket/socket.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { SocketService } from './socket.service';
-
-@Global()
-@Module({
-  providers: [SocketService],
-  exports: [SocketService],
-})
-export class SocketModule {}
