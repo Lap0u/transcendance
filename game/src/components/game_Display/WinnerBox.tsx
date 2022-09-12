b79ac2bbf4b888e4fc9af120d01df64464e9b35d
Clement BEAURAIN <clementbeaurainpro@gmail.com>
2022-09-12 12:53:09 +0200

    return to game menu at end of game ok

--- game/src/components/game_Display/WinnerBox.tsx
+++ game/src/components/game_Display/WinnerBox.tsx
@@ -1,10 +1,15 @@
+import { Button } from 'antd'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './winnerBox.css'
 
 const WinnerBox = ({message}: winnerProps) => {
+    const navigate = useNavigate();
+	
 	return (
 		<div className="container-box">
-			<p>{message}</p>
+			<p className="winner">{message}</p>
+			<Button type="dashed" onClick={() => navigate("/")}>Return to main menu</Button>
 		</div>
 	)
 }
