a5ec3269711c93da916e35e58ad6ba3f8d6205a5
Clement BEAURAIN <clementbeaurainpro@gmail.com>
2022-09-28 16:48:17 +0200

    elo fonctionne

--- back/src/game/Scores/scores.service.ts
+++ back/src/game/Scores/scores.service.ts
@@ -26,11 +26,30 @@ export class ScoresService {
     return [...player1, ...player2];
   }
 
-  async addPoint(account_id: string, points: number) {
-    const user = await this.userRepo.findOneBy({ account_id });
-    const newPoints: number = +user.points + +points;
-    return await this.userRepo.save({
-      ...user, // existing fields
+  calcPoints(winner: number, loser: number) {
+	const diff = winner - loser
+	const points = diff > 0 ? 20 - Math.round(diff / 30) : 20 + Math.round((diff * -1) / 30)
+	if (points < 0)
+		return 0
+	return points
+  }
+
+  async addPoint(winnerId: string, loserId: string) {
+	let account_id = winnerId;
+    const winner = await this.userRepo.findOneBy({ account_id });
+    account_id = loserId
+    const loser = await this.userRepo.findOneBy({ account_id });
+    const points = this.calcPoints(winner.points, loser.points)
+	console.log('points', points);
+	
+	let newPoints: number = +winner.points + +points;
+	await this.userRepo.save({
+      ...winner, // existing fields
+      points: newPoints,
+    });
+	newPoints = +loser.points - +points;
+	await this.userRepo.save({
+      ...loser, // existing fields
       points: newPoints,
     });
   }
@@ -38,7 +57,7 @@ export class ScoresService {
 
   async addScore(scores: ScoresDto) {
     const newScore = this.scoresRepo.create(scores);
-    await this.addPoint(scores.idWinner, scores.ScorePlayer1);
+    await this.addPoint(scores.idWinner, scores.idLoser);
     return this.scoresRepo.save(newScore); 
   }
 
