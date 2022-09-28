091ee365f327ab9ed53ad57fb72d4d0947d07f3d
Eli <eli.azn@hotmail.com>
2022-09-28 20:35:19 +0200

    upadated score database usernames

--- back/src/account/account/account.module.ts
+++ back/src/account/account/account.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Scores } from '../../game/Scores/entities/scores.entities';
+import { ScoresService } from '../../game/Scores/scores.service';
+import { SocketService } from '../../socket/socket.service';
 import { AuthService } from '../auth/auth.service';
 import { Accounts } from '../entities/accounts.entity';
 import DatabaseFile from '../entities/files.entity';
@@ -8,8 +11,14 @@ import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Accounts, DatabaseFile])],
+  imports: [TypeOrmModule.forFeature([Accounts, DatabaseFile, Scores])],
   controllers: [AccountController],
-  providers: [AccountService, DatabaseFilesService, AuthService],
+  providers: [
+    AccountService,
+    DatabaseFilesService,
+    AuthService,
+    SocketService,
+    ScoresService,
+  ],
 })
 export class AccountModule {}
