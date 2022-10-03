980b4e96b275cfda177afb0cfd026812e0262c14
Eli <eli.azn@hotmail.com>
2022-10-03 18:09:48 +0200

    delete get account unecessary, replaced by prop from app componant

--- game/src/components/Scores/ScorePage.tsx
+++ game/src/components/Scores/ScorePage.tsx
@@ -45,6 +45,7 @@ export function ScoresPage({currentUser}:{currentUser : typeof UserDto}){
 			.then((response) => {
 				console.log("aaazzzs", response.data);
 				getUser(response.data);
+				setOk(true);
 			})
 			.catch((error) => {
 				handleErrors(error)
